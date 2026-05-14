0a2de78991a35d81fcefe38728de6ba3f7e41aa6
igorantunes24 <igor.antunes24@escola.pr.gov.br>
2026-05-14 13:05:32 -0300

    vcg

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ textos[i].classList.add("ativo");
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2036-10-05T00:00:00");
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo){
